50d01c5953cdc04e6aa9a605d0638f4922e33c70
parimal mahato <mrpicklu13@gmail.com>
2020-05-08 03:52:51 +0530

    End of session 4

---
+++ Infrastuctore/Data/GenericRepository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastuctore.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+            throw new System.NotImplementedException();
+        }
+
+        
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+            throw new System.NotImplementedException();
+        }
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplyScecification(spec).FirstOrDefaultAsync();
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplyScecification(spec).ToListAsync();
+            throw new System.NotImplementedException();
+        }
+        private IQueryable<T>ApplyScecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(),spec);
+        }
+    }
+}
