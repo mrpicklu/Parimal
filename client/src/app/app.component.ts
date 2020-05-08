9554325cdd46c9be032cbeca75431aa9f13b8fda
parimal mahato <mrpicklu13@gmail.com>
2020-05-08 15:30:53 +0530

    Explore session 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  constructor(private http: HttpClient){}
+
+  ngOnInit():void{
+    this.http.get('http://localhost:5001/api/products').subscribe((response: any) => {
+    console.log(response);
+  }, error => {
+    console.log(error)
+    });
+  }
 }
