fee7aa2ce1799710d4d4a097e8001ef5489f7a54
parimal mahato <mrpicklu13@gmail.com>
2020-05-20 10:49:54 +0530

    End of section 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        
+        [Required]
+        [RegularExpression("(?=^.{6,255}$)((?=.*\\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))^.*",
+        ErrorMessage = "Password must have 1 uppercase, 1 Lowercase, 1 Number,1 alphanumeric and at least 6 characters")]
         public string Password { get; set; }
 
 
